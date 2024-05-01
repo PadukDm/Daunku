ed3ac5eef645b22a711345f43b6b4baef22edff5
PadukDm <146972245+PadukDm@users.noreply.github.com>
2024-05-01 19:00:44 +0300

    #

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require(`express`)
 const app = express()
 const PORT = 3000
 const mongoose = require(`mongoose`) 
+const path = require(`path`)
 
 app.use(express.static(`public`))
 app.use(express.json())
+
+app.get(`/admin`, (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public'))
+})
 
 mongoose.connect(`mongodb+srv://root:oGff1dNEIS9tbJBP@cluster0.tp3rcn1.mongodb.net/`)
     .then(()=>{
